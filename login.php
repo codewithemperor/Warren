ee6a7013af7e7d946a39b14b8736f3fe79cbe2a8
Sulaimon Yusuf <codewithemperor@gmail.com>
2025-03-22 19:30:28 +0100

    modified:   dashboard.php 	modified:   deposit.php 	modified:   index.php 	modified:   login.php 	modified:   register.php 	modified:   withdrawal.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Warren & Co - Leading Investment Company</title>
-        <link rel="canonical" href="https://warrencol.com/" />
+        
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700&amp;family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&amp;display=swap"/>
 
          <!-- Bootstrap CSS -->
